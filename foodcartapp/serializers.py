e3945fedd89dfe58901660d906c358f139bb73d4
Boyko Alexey <elorian@yandex.ru>
2022-02-18 16:33:51 +0300

    Rewrite data validation logic with DRF.

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ListField
 
-from foodcartapp.models import Product, FoodOrder
+from foodcartapp.models import FoodOrderProduct, FoodOrder
+
+
+class OrderSerialisator(ModelSerializer):
+    class Meta:
+        model = FoodOrderProduct
+        fields = ['product', 'quantity']
 
 
 class ProductSerialiser(ModelSerializer):
+    products = OrderSerialisator(many=True, allow_empty=False)
+
     class Meta:
         model = FoodOrder
-        fields = ['firstname', 'lastname', 'phonenumber', 'address']
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
+
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -65,77 +65,23 @@ def register_order(request):
     serializer = ProductSerialiser(data=request.data)
     serializer.is_valid(raise_exception=True)
     order = request.data
-    try:
-        products_order = order['products']
+    products_order = order['products']
 
-        for product in products_order:
-            if not Product.objects.filter(id__contains=product['product']):
-                content = {
-                    f'products: Недопустимый первичный ключ {product["product"]}'}
-                return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        if (not order['firstname']
-            and not order['lastname']
-            and not order['phonenumber']
-            and not order['address']
-        ):
-            content = {
-                'firstname, lastname, phonenumber, address: Это поле не может быть пустым.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        if products_order == []:
-            content = {'products: Этот список не может быть пустым.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        if not isinstance(order['firstname'], str):
-            content = {'firstname: Not a valid string.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        if not order['firstname']:
-            content = {'firstname: Это поле не может быть пустым.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        if not order['phonenumber']:
-            content = {'phonenumber: Это поле не может быть пустым.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        for digit in range(5):
-            if order['phonenumber'][digit] == '0':
-                content = {'phonenumber: Введен некорректный номер телефона'}
-                return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
+    create_order = FoodOrder.objects.create(
+        firstname=order['firstname'],
+        lastname=order['lastname'],
+        phonenumber=order['phonenumber'],
+        address=order['address']
+    )
 
-        create_order = FoodOrder.objects.create(
-            firstname=order['firstname'],
-            lastname=order['lastname'],
-            phonenumber=order['phonenumber'],
-            address=order['address']
-        )
-
-        FoodOrderProduct.objects.bulk_create(
-            [
-                FoodOrderProduct(order=create_order,
-                                 product=Product.objects.get(
-                                     id=int(product['product'])),
-                                 quantity=int(product['quantity']), )
-                for product in products_order
-            ]
-        )
-    except TypeError as error:
-        if isinstance(products_order, str):
-            content = {
-                'products: Ожидался list со значениями, но был получен "str".'}
-            return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
-        elif not products_order:
-            content = {'products: Это поле не может быть пустым.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-        else:
-            return Response(f'Ошибка - {error}', status.HTTP_400_BAD_REQUEST)
-
-    except KeyError as error:
-        if ('firstname' not in order
-            and 'lastname' not in order
-            and 'phonenumber' not in order
-            and 'address' not in order
-        ):
-            content = {
-                'firstname, lastname, phonenumber, address: Обязательное поле.'}
-            return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
-
-        content = {'products: Обязательное поле.'}
-        return Response(content, status.HTTP_406_NOT_ACCEPTABLE)
+    FoodOrderProduct.objects.bulk_create(
+        [
+            FoodOrderProduct(order=create_order,
+                             product=Product.objects.get(
+                                 id=int(product['product'])),
+                             quantity=int(product['quantity']), )
+            for product in products_order
+        ]
+    )
 
     return JsonResponse({})
