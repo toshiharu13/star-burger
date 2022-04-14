550ad0419207b8b9153e3eedfcac77e2b833b5e9
Boyko Alexey <elorian@yandex.ru>
2022-04-14 17:27:25 +0300

    Raname some variables in <register_order> func

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -70,7 +70,7 @@ def register_order(request):
     serializer.is_valid(raise_exception=True)
     products_order = serializer.validated_data['products']
 
-    create_order = FoodOrder.objects.create(
+    new_order = FoodOrder.objects.create(
         firstname=serializer.validated_data['firstname'],
         lastname=serializer.validated_data['lastname'],
         phonenumber=serializer.validated_data['phonenumber'],
@@ -79,7 +79,7 @@ def register_order(request):
 
     FoodOrderProduct.objects.bulk_create(
         [
-            FoodOrderProduct(order=create_order,
+            FoodOrderProduct(order=new_order,
                              product=product['product'],
                              quantity=(product['quantity']),
                              price=Product.objects.get(
@@ -88,31 +88,30 @@ def register_order(request):
         ]
     )
 
-    allrestaurantmenu = RestaurantMenuItem.objects.select_related(
+    all_restaurants_menu = RestaurantMenuItem.objects.select_related(
         'restaurant').select_related('product').all()
-    all_restaurants = []
+    splitted_suitable_restaurants = []
     suitable_restaurant = []
-    all_order_in_one = False
 
     for product in products_order:
-        need_restaurants = allrestaurantmenu.filter(
+        suitable_restaurants = all_restaurants_menu.filter(
             product__name=product['product'])
-        one_burger_restaurants = []
-        for need_restaurant in need_restaurants:
-            one_burger_restaurants.append(need_restaurant.restaurant.name)
-        all_restaurants.append(one_burger_restaurants)
+        sorted_by_product_restaurants = []
+        for suitable_restaurant in suitable_restaurants:
+            sorted_by_product_restaurants.append(suitable_restaurant.restaurant.name)
+        splitted_suitable_restaurants.append(sorted_by_product_restaurants)
 
-    if all_restaurants:
-        first_burger_restaurants = all_restaurants[0]
+    if splitted_suitable_restaurants:
+        first_burger_restaurants = splitted_suitable_restaurants[0]
         for first_burger_restaurant in first_burger_restaurants:
-            for current_burger_restaurants in all_restaurants:
+            for current_burger_restaurants in splitted_suitable_restaurants:
                 if first_burger_restaurant not in current_burger_restaurants:
                     continue
             suitable_restaurant.append(first_burger_restaurant)
 
         for restaurant in suitable_restaurant:
             restuarant_object = get_object_or_404(Restaurant, name=restaurant)
-            create_order.recommended_restaurants.add(restuarant_object)
-        create_order.save()
+            new_order.recommended_restaurants.add(restuarant_object)
+        new_order.save()
 
     return Response(serializer.data, status=201)
