33ac80ddc0756681008ea8c92be22f5798c421ed
Boyko Alex <elorian@yandex.ru>
2022-05-19 12:24:46 +0000

    Correct frontend modul

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -13,7 +13,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY', 'etirgvonenrfnoerngorenogneongg334g')
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
