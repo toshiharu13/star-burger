acab20970b02fada0966b86b39dd6567de519aa8
Boyko Alex <elorian@yandex.ru>
2022-05-26 15:34:31 +0300

    Complite deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 set -e
-
+# Обновит код репозитория
 git pull
-echo "Pull complite"
+# Установит библиотеки python
+./venv/bin/pip install -r requirements.txt
+# Установит библиотеки node.js
+npm ci --dev
+# Пересоберёт JS-код
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+# Пересоберёт статику Django
+./venv/bin/python manage.py  collectstatic
+# Накатит миграции
+./venv/bin/python manage.py migrate
+# Перезапустит сервисы Systemd
+systemctl restart django-burger.service
+# Сообщит об успешном завершении деплоя
+echo "----------------"
+echo "Deploy complite!"
+echo "----------------"
+
