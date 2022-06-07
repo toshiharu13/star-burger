9b43c824c944a95461a26759754937f12a125985
Boyko Alexey <elorian@yandex.ru>
2022-06-07 15:30:32 +0300

    Add --noinput to collectctatic comand and comments in deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
 set -e
-# Обновит код репозитория
+
+echo "Update from repo"
 git pull
-# Установит библиотеки python
+
+echo "Install requirements"
 ./venv/bin/pip install -r requirements.txt
-# Установит библиотеки node.js
+
+echo "Install node.js libs"
 npm ci --dev
-# Пересоберёт JS-код
+
+echo "Rebuild JS-code"
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-# Пересоберёт статику Django
-./venv/bin/python manage.py  collectstatic
-# Накатит миграции
+
+echo "Collect Django static"
+./venv/bin/python manage.py  collectstatic --noinput
+
+echo "Make migrations"
 ./venv/bin/python manage.py makemigrations
 ./venv/bin/python manage.py migrate
-# Перезапустит сервисы Systemd
+
+echo "restart Systemd service"
 systemctl restart django-burger.service
-#  Передача в rollbar.com информации о деплое
+
+echo  "Send info about deploy to rollbar.com"
 comit_number=$(git rev-parse --short HEAD)
 curl -H "X-Rollbar-Access-Token: ffd0c1e6e6d44392be42d0e83821f8d4" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "status": "succeeded","revision": $comit_number, "user": "Toshiharu"}'
 
-# Сообщит об успешном завершении деплоя
 echo "----------------"
 echo "Deploy complete!"
 echo "----------------"
