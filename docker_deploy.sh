70098224287e29681df02026c55c61de605f739a
elf <elorian@yandex.ru>
2022-07-01 22:59:46 +0300

    Create sh for docker deploy

---
+++ docker_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "Update from repo"
+git pull
+
+echo "Rebuild docker containers"
+docker-compose up --build
+
+echo  "Send info about deploy to rollbar.com"
+http POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:ffd0c1e6e6d44392be42d0e83821f8d4 environment=production revision=$(git rev-parse --short HEAD) rollbar_name=FirstProject local_username=Toshiharu
+
+echo "----------------"
+echo "Deploy complete!"
+echo "----------------"
