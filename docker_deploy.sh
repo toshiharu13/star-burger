ce2fb0efe26fd69ea9bfc5bc0a4c8492a4710775
elf <elorian@yandex.ru>
2022-07-01 23:35:20 +0300

    Add detouch to build docker

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -6,7 +6,7 @@ echo "Update from repo"
 git pull
 
 echo "Rebuild docker containers"
-docker-compose up --build
+docker-compose up --build -d
 
 echo  "Send info about deploy to rollbar.com"
 http POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:ffd0c1e6e6d44392be42d0e83821f8d4 environment=production revision=$(git rev-parse --short HEAD) rollbar_name=FirstProject local_username=Toshiharu
