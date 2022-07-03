60b1ed5dbfd3e38777f41bd5815ffadf4d107f69
elf <elorian@yandex.ru>
2022-07-03 19:05:39 +0300

    Correct docker_deploy.sh

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -5,6 +5,22 @@ set -e
 echo "Update from repo"
 git pull
 
+echo "Install requirements"
+./venv/bin/pip install -r  requirements.txt --no-input
+
+echo "Install node.js libs"
+npm ci --dev
+
+echo "Rebuild JS-code"
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+echo "Collect Django static"
+./venv/bin/python manage.py  collectstatic --noinput
+
+echo "Make migrations"
+./venv/bin/python manage.py makemigrations --noinput
+./venv/bin/python manage.py migrate --noinput
+
 echo "Rebuild docker containers"
 docker-compose up --build -d
 
